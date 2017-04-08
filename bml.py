3ca643c50c07e57e87b8964846dc5aaada3f6ec7
Evgeniy Zheltonozhskiy <zheltonozhskiy@gmail.com>
2017-04-08 17:29:55 +0300

    Speedup using indexes lists

--- bml.py
+++ bml.py
@@ -10,6 +10,7 @@ colormap = plt.get_cmap('inferno')
 colors = [colormap(k) for k in np.linspace(0.0, 0.8, num_of_colors)]
 sec_colors = [colormap(k + 0.1) for k in np.linspace(0.0, 0.8, num_of_colors)]
 
+
 class Cell(IntEnum):
     EMPTY = 0
     RIGHT = 1
@@ -28,13 +29,13 @@ class BML:
         
         num_of_cars = int(width * height * (p / 2))
         # Get a list of indices
-        indices = list(np.ndindex(self.cells.shape))
+        inds = list(np.ndindex(self.cells.shape))
         # Shuffle the indices in-place
-        np.random.shuffle(indices)
+        np.random.shuffle(inds)
         # Access array elements using the indices to do cool stuff
-        for i in indices[:num_of_cars]:
+        for i in inds[:num_of_cars]:
             self.cells[i] = Cell.RIGHT
-        for i in indices[num_of_cars:2 * num_of_cars]:
+        for i in inds[num_of_cars:2 * num_of_cars]:
             self.cells[i] = Cell.DOWN
         
         self.total_number = 2 * num_of_cars
@@ -43,39 +44,28 @@ class BML:
         if self.model == 1:
             vel = self.step_right()
             vel += self.step_down()
-            self.velocity.append(vel / (self.width * self.height))
+            self.velocity.append(vel / self.total_number)
         elif self.model == 2:
-            self.velocity.append(self.step_all() / (self.width * self.height))
+            self.velocity.append(self.step_all() / self.total_number)
         elif self.model == 3:
             self.step_all_together()  # TODO
         self.step += 1
     
     def step_down(self):
-        next_state = self.cells
-        count = 0
-        for i in range(self.height):
-            for j in range(self.width):
-                if self.cells[self.upper(i, j)] == Cell.DOWN and self.cells[(i, j)] == Cell.EMPTY:
-                    next_state[(i, j)] = Cell.DOWN
-                    next_state[self.upper(i, j)] = Cell.EMPTY
-                    count += 1
-        self.cells = next_state
-        return count
-    
-    def step_all_together(self):
-        return -1
+        d = np.diff(np.r_[self.cells, [self.cells[0, :]]], axis=0)
+        down_indexes = np.where(np.logical_and(self.cells == Cell.DOWN, d == Cell.EMPTY - Cell.DOWN))
+        empty_indexes = self.down_set(down_indexes)
+        self.cells[down_indexes] = Cell.EMPTY
+        self.cells[empty_indexes] = Cell.DOWN
+        return len(down_indexes[0])
     
     def step_right(self):
-        next_state = self.cells
-        count = 0
-        for i in range(self.height):
-            for j in range(self.width):
-                if self.cells[self.left(i, j)] == Cell.RIGHT and self.cells[(i, j)] == Cell.EMPTY:
-                    next_state[(i, j)] = Cell.RIGHT
-                    next_state[self.left(i, j)] = Cell.EMPTY
-                    count += 1
-        self.cells = next_state
-        return count
+        d = np.diff(np.c_[self.cells, self.cells[:, 0]])
+        right_indexes = np.where(np.logical_and(self.cells == Cell.RIGHT, d == Cell.EMPTY - Cell.RIGHT))
+        empty_indexes = self.right_set(right_indexes)
+        self.cells[right_indexes] = Cell.EMPTY
+        self.cells[empty_indexes] = Cell.RIGHT
+        return len(right_indexes[0])
     
     def step_all(self):
         next_state = self.cells
@@ -101,12 +91,33 @@ class BML:
                         count += 1
         self.cells = next_state
         return count
+
+    def step_all_together(self):
+        return -1
     
     def upper(self, i, j):
         return self.height - 1 if i == 0 else i - 1, j
     
     def left(self, i, j):
         return i, self.width - 1 if j == 0 else j - 1
+
+    def upper_t(self, ind):
+        return self.height - 1 if ind[0] == 0 else ind[0] - 1, ind[1]
+
+    def left_t(self, ind):
+        return ind[0], self.width - 1 if ind[1] == 0 else ind[1] - 1
+
+    def down_t(self, ind):
+        return 0 if ind[0] == self.height - 1 else ind[0] + 1, ind[1]
+
+    def right_t(self, ind):
+        return ind[0], 0 if ind[1] == self.width - 1 else ind[1] + 1
+
+    def down_set(self, inds):
+        return [tuple((x + 1) % self.height for x in inds[0]), inds[1]]
+
+    def right_set(self, inds):
+        return [inds[0], tuple((x + 1) % self.width for x in inds[1])]
     
     def run(self, steps):
         start = time.perf_counter()
@@ -139,25 +150,26 @@ class BML:
         rcParams.update(params)
         fig = figure()  # no frame
         ax = fig.add_subplot(111)
-        ax.plot(self.velocity, linewidth=1, color=colors[0])
+        ax.plot(self.velocity, linewidth=1, color=colors[1])
         ax.set_ylim(0, 1)
-    
+        ax.set_yticks(np.arange(0, 1, 0.2))
+        
         ax.get_xaxis().tick_bottom()
         ax.get_yaxis().tick_left()
-    
+
         ax.set_xlabel('Step')
         ax.set_ylabel('Velocity')
-    
+
         ax.grid(axis='y', color="0.9", linestyle='-', linewidth=1)
         ax.set_axisbelow(True)
-    
+
         plt.tight_layout()
-    
+
         fig.savefig('velocity.png', dpi=600)
 
 
-automat = BML(128, 128, 0.5, 1)
+automat = BML(521, 523, 0.2, 1)
 automat.save()
-automat.run(1000)
+automat.run(3500)
 automat.save()
 automat.plot_velocity()
